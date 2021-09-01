daa70e579d2b2a2cf7e35305b2dc4df7249f700f
kboxstar@gmail.com <psg980727!>
2021-09-01 21:08:36 +0900

    MYSQL DB Connection Test

---
+++ src/main/java/com/hjkill/spring/base/common/DBConnectionTest.java
@@ -0,0 +1,24 @@
+package com.hjkill.spring.base.common;
+
+import java.sql.*;
+
+public class DBConnectionTest {
+    public static void main(String[] args){
+        try{
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306?serverTimezone=UTC",
+                    "root",
+                    ""
+            );
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(
+                    "show databases"
+            );
+            while (rs.next()){
+                System.out.println(rs.getString(1));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
